6b7e3406edb31a9ccc083c8d3d3f42df93fcd4fa
Amey <ameydeshingkar@gmail.com>
2021-09-19 10:55:18 +0530

    Search Bar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,37 @@
+/** @format */
+
+import React from "react";
+import { StyleSheet, TextInput, View } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+const SearchBar = () => {
+	return (
+		<View style={styles.backgroundStyle}>
+			<Feather name="search" style={styles.iconStyle} />
+			<TextInput placeholder="Search" style={styles.inputStyle} />
+		</View>
+	);
+};
+
+export default SearchBar;
+
+const styles = StyleSheet.create({
+	backgroundStyle: {
+		marginTop: 10,
+		backgroundColor: "#F0EEEE",
+		height: 50,
+		borderRadius: 10,
+		marginHorizontal: 15,
+		flexDirection: "row",
+	},
+	inputStyle: {
+		flex: 1,
+		fontSize: 18,
+	},
+	iconStyle: {
+		fontSize: 35,
+		alignSelf: "center",
+		marginRight: 15,
+		marginLeft: 5,
+	},
+});
